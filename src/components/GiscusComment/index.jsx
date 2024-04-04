fff591f390dbb8e4823477db7388bf6e0f25bdf9
zephyr-sh <shaynling@gmail.com>
2024-04-04 10:50:09 +0800

    [C] Update margin for `GiscusComment`

--- src/components/GiscusComment/index.jsx
+++ src/components/GiscusComment/index.jsx
@@ -6,21 +6,23 @@ export default function GiscusComment() {
     const { colorMode } = useColorMode();
 
     return (
-        <Giscus
-            repo="DocsaidLab/blog"
-            repoId="R_kgDOK0_How"
-            category="Announcements"
-            categoryId="DIC_kwDOK0_Ho84CeZ07"
-            mapping="title"
-            strict="0"
-            reactionsEnabled="1"
-            emitMetadata="0"
-            inputPosition="top"
-            theme={colorMode}
-            lang="zh-TW"
-            loading="lazy"
-            crossorigin="anonymous"
-            async
-        />
+        <div style={{ marginTop: '3rem' }}> {/* 增加邊距 */}
+            <Giscus
+                repo="DocsaidLab/blog"
+                repoId="R_kgDOK0_How"
+                category="Announcements"
+                categoryId="DIC_kwDOK0_Ho84CeZ07"
+                mapping="title"
+                strict="0"
+                reactionsEnabled="1"
+                emitMetadata="0"
+                inputPosition="top"
+                theme={colorMode}
+                lang="zh-TW"
+                loading="lazy"
+                crossorigin="anonymous"
+                async
+            />
+        </div>
     );
 }
