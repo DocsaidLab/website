37a15293d4a12338644599b1374faa1aa6bbb450
zephyr-sh <shaynling@gmail.com>
2024-06-10 14:05:18 +0800

    [C] Update `trailingSlash` setting to false

--- update_docsearch.sh
+++ update_docsearch.sh
@@ -11,7 +11,11 @@ CONFIG=$(cat <<EOF
   "start_urls": ["https://docsaid.org/"],
   "sitemap_urls": ["https://docsaid.org/sitemap.xml"],
   "sitemap_alternate_links": true,
-  "stop_urls": ["/tests"],
+  "stop_urls": [
+    "/tests",
+    "/search",
+    ".*/#.*"
+  ],
   "selectors": {
     "lvl0": {
       "selector": "(//ul[contains(@class,'menu__list')]//a[contains(@class, 'menu__link menu__link--sublist menu__link--active')]/text() | //nav[contains(@class, 'navbar')]//a[contains(@class, 'navbar__link--active')]/text())[last()]",
@@ -19,7 +23,7 @@ CONFIG=$(cat <<EOF
       "global": true,
       "default_value": "Documentation"
     },
-    "lvl1": "header h1",
+    "lvl1": "header h1, article h1",
     "lvl2": "article h2",
     "lvl3": "article h3",
     "lvl4": "article h4",
@@ -27,10 +31,14 @@ CONFIG=$(cat <<EOF
     "lvl6": "article h6",
     "text": "article p, article li, article td:last-child"
   },
-  "strip_chars": " .,;:#",
   "custom_settings": {
     "separatorsToIndex": "_",
-    "attributesForFaceting": ["language", "version", "type", "docusaurus_tag"],
+    "attributesForFaceting": [
+      "filterOnly(language)",
+      "filterOnly(version)",
+      "filterOnly(type)",
+      "filterOnly(docusaurus_tag)"
+    ],
     "attributesToRetrieve": [
       "hierarchy",
       "content",
@@ -40,8 +48,16 @@ CONFIG=$(cat <<EOF
       "type"
     ]
   },
-  "conversation_id": ["833762294"],
-  "nb_hits": 46250
+  "searchableAttributes": [
+    "hierarchy.lvl0",
+    "hierarchy.lvl1",
+    "hierarchy.lvl2",
+    "hierarchy.lvl3",
+    "hierarchy.lvl4",
+    "hierarchy.lvl5",
+    "hierarchy.lvl6",
+    "content"
+  ]
 }
 EOF
 )
